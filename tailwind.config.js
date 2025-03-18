c189bb26ec0f224d973e083c5a32c722c6847ba7
Tricia Nshuti <nshutricia@gmail.com>
2025-03-18 22:56:02 +0200

    feat: In progress mobile view port !

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'iphone-small': '393px', // iPhone 14 & 15 Pro Max
+        'ipad': '834px',   // iPad 11 Pro
+        'macbook': '1440px', // MacBook 14 Pro
+      },
+    },
   },
   plugins: [],
 };
