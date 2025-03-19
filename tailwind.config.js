ad3ab9e6b381f910f7af7ec3f8bf9081b132b460
Tricia Nshuti <nshutricia@gmail.com>
2025-03-20 01:29:27 +0200

    feat: Completion of the given viewport responsiveness regarding hero shapes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      screens: {
-        'iphone-small': '393px', // iPhone 14 & 15 Pro Max
-        'ipad': '834px',   // iPad 11 Pro
-        'macbook': '1440px', // MacBook 14 Pro
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
